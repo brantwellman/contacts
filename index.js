3b4578eab575c91b20796ef46f8157c12d47ed3a
Brant Wellman <BrantWellman@gmail.com>
2016-08-17 13:34:43 -0600

    Add functionality for contacts to be rendered dynamically

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-var oldHello = function(name) {
-  console.log('hello, ' + name + '!');
-};
+// var oldHello = function(name) {
+//   console.log('hello, ' + name + '!');
+// };
+//
+//
+// const hello = (name) => {
+//   console.log(`hello, ${name}!`);
+// };
+//
+// oldHello('world');
+// hello('es6');
+
+$(document).ready(function() {
+  appendContactList(contacts);
+});
 
 
-const hello = (name) => {
-  console.log(`hello, ${name}!`);
-};
+// Dynamic Rendering of Contacts
 
-oldHello('world');
-hello('es6');
+function appendContactList(contacts){
+  contacts.forEach(renderContact);
+}
+
+function renderContact(contact){
+  $('.contact-list').append(
+    compileContact(contact)
+  );
+}
+
+function compileContact(contact){
+  return "<div class='contact'><p class='name'>" + contact + "</p></div>";
+}
+
+var contacts = [
+  'Andy Mention',
+  'Emily Davis',
+  'Eric Fransen',
+  'Jessica Goulding',
+  'Jonmichael Chambers',
+  'Marc Garreau',
+  'Tan Doan',
+  'Alan Smith',
+  'Allison Larson',
+  'Andrew Watkins',
+  'Chad Brading',
+  'Corey Davis',
+  'Gustavo Villagrana',
+  'Hilary Denton',
+  'Horacio Chavez',
+  'Tim Proctor',
+  'Wil Faurot'
+];
